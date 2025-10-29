13f2addd0bf6ca688d3aa5f6390cd225f5f23168
Wadswow <nicholas102spy@gmail.com>
2025-10-29 02:17:09 +0000

    Using Event to draw the canvas and have observers in code for when there is a change to the canvas (Step 3 complete)

--- src/main.ts
+++ src/main.ts
@@ -13,25 +13,46 @@ drawingBoard.height = 256;
 document.body.append(drawingBoard);
 const drawing = drawingBoard.getContext("2d")!;
 const pen = { active: false, x: 0, y: 0 };
+const strokes: Array<Array<{ x: number; y: number }>> = [];
+let currentStroke: Array<{ x: number; y: number }> | null = null;
 
 //drawing logic
+drawingBoard.addEventListener("change", (_e) => {
+  drawing.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
+  for (const line of strokes) {
+    if (line.length > 1) {
+      drawing.beginPath();
+      const { x, y } = line[0];
+      drawing.moveTo(x, y);
+      for (const { x, y } of line) {
+        drawing.lineTo(x, y);
+      }
+      drawing.stroke();
+    }
+  }
+});
+
 drawingBoard.addEventListener("mousedown", (e) => {
   pen.active = true;
   pen.x = e.offsetX;
   pen.y = e.offsetY;
+  currentStroke = [];
+  strokes.push(currentStroke);
+  currentStroke.push({ x: pen.x, y: pen.y });
+  drawingBoard.dispatchEvent(new Event("change"));
 });
 drawingBoard.addEventListener("mousemove", (e) => {
   if (pen.active) {
-    drawing.beginPath();
-    drawing.moveTo(pen.x, pen.y);
-    drawing.lineTo(e.offsetX, e.offsetY);
-    drawing.stroke();
     pen.x = e.offsetX;
     pen.y = e.offsetY;
+    currentStroke!.push({ x: pen.x, y: pen.y });
+    drawingBoard.dispatchEvent(new Event("change"));
   }
 });
 drawingBoard.addEventListener("mouseup", (_e) => {
   pen.active = false;
+  currentStroke = null;
+  drawingBoard.dispatchEvent(new Event("change"));
 });
 
 //clear button elements
@@ -43,4 +64,5 @@ document.body.append(clearButton);
 //clear button logic
 clearButton.addEventListener("click", () => {
   drawing.clearRect(0, 0, drawingBoard.width, drawingBoard.height);
+  strokes.splice(0, strokes.length);
 });
